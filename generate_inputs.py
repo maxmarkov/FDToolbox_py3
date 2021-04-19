cd29bd526ba7f459dd50c2dd7937e45ac8c2c06c
Maksim Markov (maksim.markov@uclouvain.be) <mmarkov@mbackf1.cism.ucl.ac.be>
2021-04-20 00:04:02 +0200

    generator restructured

--- generate_inputs.py
+++ generate_inputs.py
@@ -5,17 +5,19 @@ import sys
 import os
 import argparse
 
-#from pymatgen.io.vasp.sets import MPStaticSet
+from pymatgen.io.vasp.sets import MPStaticSet
 from pymatgen.transformations.site_transformations import TranslateSitesTransformation
 from pymatgen.core.structure import Structure
 from pymatgen.core.lattice import Lattice
 
+
+
 def move_all_atoms(structure, disp_ion):
+  """
+  Create structures with displaced ions
+  """
   yield 'calc_00_0', structure
   print(structure.lattice)
-  if disp_ion == 0.:
-     print("Ionic displacement is zero. Only the reference structure is returned.\n") 
-     return
   atoms = [i for i in range(len(structure))]
   for atom in atoms:
     for direction in range(3):
@@ -29,9 +31,9 @@ def move_all_atoms(structure, disp_ion):
          displacement[direction] = -displacement[direction]
 
 def move_all_lattice(structure, disp_lattice):
-  if disp_lattice == 0.:
-     print("Lattice displacement is zero. Strain contribution won't be computed.\n")
-     return
+  """
+  Create structures with displaced lattice
+  """
   for axnum, axname in enumerate(['a', 'b', 'c']):
     for dirnum, dirname in enumerate(['X', 'Y', 'Z']):
       for orientation in ['+', '-']:
@@ -40,32 +42,67 @@ def move_all_lattice(structure, disp_lattice):
          yield 'calc_00_%s%s%s' % (axname, orientation, dirname), structure_displaced
          disp_lattice = -disp_lattice
 
-def generate_all_calcs(structure, disp_ion, disp_lattice):
-  for name, c in move_all_atoms(structure, disp_ion):
-    yield name, c
-  for name, c in move_all_lattice(structure, disp_lattice):
-    yield name, c
+def generate_scf_folders(structures, user_incar):
+  """
+  Generate calculations from the structures dictionary 
+  """
+  for name in structures:
+      if os.path.exists(name)==False:
+         os.mkdir(name)
+      structures[name].to(fmt='POSCAR', filename='/'.join([name, 'POSCAR']))
+      MPStaticSet(structures[name], user_incar).write_input(name)
+      print(name+'\n')
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--filename', type=str, default='POSCAR', help='File with original structure. POSCAR or cif formats')
-    parser.add_argument('--disp_ion', type=float, default='0.005', help='Magnitude of ionic displacement')
-    parser.add_argument('--disp_lattice', type=float, default='0.000', help='Magnitude of lattice vectors displacements')
-    opt = parser.parse_args()
 
-    # customize the default settings for INCAR file
-    user_incar_custom = {"ALGO":"Normal","LDAUU":{"Eu":6.0},"LDAUL":{"Eu":3},"EDIFF":5e-8,"LWAVE":False,"LVHAR":False,"LAECHG":False,"NPAR":4,"ICHARG":2} 
 
-    if os.path.isfile(opt.filename) == False:
-       sys.exit('Correct path to a structure POSCAR/cif file must be provided after --filename')
+class FDTcalculations(object):
+    """
+    This class generates the self-consistent calculations for FDToolbox 
+    """
+    def __init__(self, filename='POSCAR', disp_ion=0.005, disp_lattice=0.000):
+       """
+       Initializes the class from the original structure file and displacement amplitudes
+       """
+       self.filename = filename
+       self.disp_ion = disp_ion
+       self.disp_lattice = disp_lattice 
+       if os.path.isfile(filename) == False:
+          sys.exit('Correct path to a structure POSCAR/cif file must be provided')
     
-    structure = Structure.from_file(opt.filename)
+       self.structure = Structure.from_file(self.filename)
+       print(self.structure)
 
-    for name, structure_displaced in generate_all_calcs(structure, opt.disp_ion, opt.disp_lattice):
-        try:
-          os.mkdir(name)
-        except:
-          pass
-        structure_displaced.to(fmt='POSCAR', filename='/'.join( [name, 'POSCAR']))
-        print(name)
-        #MPStaticSet(structure, user_incar_custom).write_input(name)
+       # dictionary with ionic displacements
+       self.structures_ion = {}
+       if self.disp_ion > 0.0:
+          for name, structure_displaced in move_all_atoms(self.structure, self.disp_ion):
+              self.structures_ion[name] = structure_displaced
+       else:
+          print("Ionic displacement is zero. Only the reference structure is returned.\n")
+
+       # dictionary with lattice displacements
+       self.structures_lattice = {}
+       if self.disp_lattice > 0.0:
+          for name, structure_displaced in move_all_lattice(self.structure, self.disp_lattice):
+              self.structures_lattice[name] = structure_displaced
+       else:
+          print("Lattice displacement is zero. Strain contribution won't be computed.\n")
+
+    def generate_scf(self, user_incar={"ALGO":"Normal","EDIFF":5e-8,"LWAVE":False,"LVHAR":False,"LAECHG":False,"NPAR":4,"ICHARG":2}):
+        """
+        Generate folders with calculations: INCAR, POSCAR, POTCAR, KPOINTS
+        """
+        if len(self.structures_ion) > 0:
+           generate_scf_folders(self.structures_ion, user_incar)
+        if len(self.structures_lattice) > 0:
+           generate_scf_folders(self.structures_lattice, user_incar)
+
+
+############################################
+###         EXAMPLE HOW TO RUN           ###
+############################################
+# customize the default settings for INCAR file
+user_incar_custom = {"ALGO":"Normal","LDAUU":{"Eu":6.0},"LDAUL":{"Eu":3},"EDIFF":5e-8,"LWAVE":False,"LVHAR":False,"LAECHG":False,"NPAR":4,"ICHARG":2}
+
+fdt = FDTcalculations(filename='../FDToolbox_py3_dev/TEST_FOLDER/POSCAR', disp_ion=0.005, disp_lattice=0.00)
+fdt.generate_scf(user_incar=user_incar_custom)
