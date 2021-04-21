bb47c2e3c923118097102947842efeb3e512de21
Maksim Markov (maksim.markov@uclouvain.be) <mmarkov@mbackf1.cism.ucl.ac.be>
2021-04-21 20:05:34 +0200

    soc generator

--- generate_inputs.py
+++ generate_inputs.py
@@ -6,7 +6,8 @@ import os
 import argparse
 import shutil
 
-from pymatgen.io.vasp.sets import MPStaticSet
+from pymatgen.io.vasp.sets import MPStaticSet, MPSOCSet
+from pymatgen.io.vasp.outputs import Outcar
 from pymatgen.transformations.site_transformations import TranslateSitesTransformation
 from pymatgen.core.structure import Structure
 from pymatgen.core.lattice import Lattice
@@ -53,6 +54,17 @@ def generate_scf_folders(structures, user_incar):
         MPStaticSet(structures[name], user_incar_settings=user_incar, user_kpoints_settings={"reciprocal_density": 500}).write_input(name)
         print(name+'\n')
 
+def check_files(structures, filename):
+    """
+    Check that filename exists and not empty in all structures folders
+    """
+    calcs_failed = []
+    for name in structures:
+        f = os.path.join(name, filename)
+        if os.path.exists(f) == False or os.path.getsize(f) < 1000:
+           calcs_failed.append(name)
+    return(calcs_failed)
+
 def generate_old_polarization_folders(structures):
     """
     Generate the polarization calculations (old format) from the completed scf calculation
@@ -61,9 +73,34 @@ def generate_old_polarization_folders(structures):
         for i in range(3):
             # compute the Berry phase along the x-i, y- and z-directions (IGPAR = 1,2,3)
             # Note: calculations usualy are very sensetive to the choice of "NPPSTR" parameter. You might consider to reduce it.
-            MPStaticSet.from_prev_calc(name, user_incar_settings={"LBERRY":True,"IGPAR":i+1,"ICHARG":2,"NPAR":2,"ALGO":"Fast","NPPSTR":6,"LAECHG":False,"LVHAR":False, "ISIF": 2}).write_input(os.path.join(name,'Berry_'+str(i+1)))
+            MPStaticSet.from_prev_calc(name, user_incar_settings={"LBERRY":True,"IGPAR":i+1,"ICHARG":2,"NPAR":2,"ALGO":"Fast","NPPSTR":6,"LAECHG":False,"LVHAR":False, "ISIF": 2}, user_kpoints_settings={"reciprocal_density": 500}).write_input(os.path.join(name,'Berry_'+str(i+1)))
             #shutil.copy(os.path.join(name,'CHGCAR'), os.path.join(name,'Berry_'+str(i+1)))
-            shutil.copy(os.path.join(name,'KPOINTS'), os.path.join(name,'Berry_'+str(i+1)))
+        break
+
+def generate_new_polarization_folders(structures):
+    """
+    Generate the polarization calculations (new format) from the completed scf calculation
+    """
+    for name in structures:
+        MPStaticSet.from_prev_calc(name, user_incar_settings={"LCALCPOL":True,"LAECHG":False,"LVHAR":False, "ISIF": 2}, user_kpoints_settings={"reciprocal_density": 500}).write_input(os.path.join(name,'Berry_new'))
+        #shutil.copy(os.path.join(name,'CHGCAR'), os.path.join(name,'Berry_'+str(i+1)))
+        break
+
+def generate_nscf_soc_folders(structures, saxis, user_incar):
+    """
+    Generate the non self-consistent calculation with the spin-orbit coupling 
+    """
+    for name in structures:
+        # add magmom as a site property for each atom
+        outcar = Outcar(filename=os.path.join(name,'OUTCAR'))
+        for i, atom in enumerate(structures[name].sites):
+            atom.properties={"magmom":[0.,0., round(outcar.magnetization[i]['tot'],2)]}
+        # geterate calculation from the MPSOCSet 
+        mpsoc = MPSOCSet(structures[name], saxis=saxis, user_incar_settings=user_incar, user_kpoints_settings={"reciprocal_density": 500})
+        mpsoc.write_input(os.path.join(name,'nscf_SOC'))
+        shutil.copy(os.path.join(name,'CHGCAR'), os.path.join(name,'nscf_SOC'))
+        break
+
 
 
 class FDTcalculations(object):
@@ -99,37 +136,33 @@ class FDTcalculations(object):
        else:
           print("Lattice displacement is zero. Strain contribution won't be computed.\n")
 
-    def generate_scf(self, user_incar={"ALGO":"Normal","EDIFF":5e-8,"LWAVE":False,"LVHAR":False,"LAECHG":False,"NPAR":4,"ICHARG":2}):
+    def generate_scf(self, user_incar_custom):
         """
         Generate folders with calculations: INCAR, POSCAR, POTCAR, KPOINTS
         """
+        user_incar={"ALGO":"Normal","EDIFF":5e-8,"LWAVE":False,"LVHAR":False,"LAECHG":False,"NPAR":4,"ICHARG":2}
         if len(self.structures_ion) > 0:
-           generate_scf_folders(self.structures_ion, user_incar)
+           generate_scf_folders(self.structures_ion, {**user_incar, **user_incar_custom})
         if len(self.structures_lattice) > 0:
-           generate_scf_folders(self.structures_lattice, user_incar)
+           generate_scf_folders(self.structures_lattice, {**user_incar, **user_incar_custom})
 
     def check_scf(self):
         """
         Check either all scf calculations have been completed correctly 
         """
-        calcs_failed = []
+        calcs_failed_ion = []
         if len(self.structures_ion) > 0:
-           for folder in self.structures_ion:
-               f = os.path.join(folder,'CHGCAR')
-               if os.path.exists(f) == False or os.path.getsize(f) < 1000:
-                  calcs_failed.append(folder)
-                  print('Calculation in {} folder has not been completed'.format(folder))
+           calcs_failed_ion = check_files(self.structures_ion, 'CHGCAR')
+        calcs_failed_lattice = []
         if len(self.structures_lattice) > 0:
-           for folder in self.structures_lattice:
-               f = os.path.join(folder,'CHGCAR')
-               if os.path.exists(f) == False or os.path.getsize(f) < 1000:
-                  calcs_failed.append(folder)
+           calcs_failed_lattice = check_files(self.structures_lattice, 'CHGCAR')
+
+        calcs_failed = calcs_failed_ion + calcs_failed_lattice
         if len(calcs_failed) == 0:
            return(True)
         else:
            print('Following calculations have problems: ', calcs_failed)
            return(False)
-
 
     def generate_old_polarization(self):
         """
@@ -140,13 +173,32 @@ class FDTcalculations(object):
         if len(self.structures_lattice) > 0:
            generate_old_polarization_folders(self.structures_lattice)
 
+    def generate_new_polarization(self):
+        """
+        Generate folders with (new-type) polarization calculations
+        """
+        if len(self.structures_ion) > 0:
+           generate_new_polarization_folders(self.structures_ion)
+        if len(self.structures_lattice) > 0:
+           generate_new_polarization_folders(self.structures_lattice)
+
+    def generate_nscf_soc(self, saxis, user_incar_custom):
+        """
+        Generate folders with non self-consistent calculations with spin-orbit coupling
+        """
+        user_incar={"ICHARG":1, "ALGO":"Normal", "EDIFF":5e-8, "NELMDL":15, "LCHARG":False, "LWAVE":False, "LVHAR":False, "LAECHG":False, "NPAR":2, "LREAL":False, "ISYM":-1, "ISIF": 2}
+        if len(self.structures_ion) > 0:
+           generate_nscf_soc_folders(self.structures_ion, saxis, {**user_incar, **user_incar_custom})
+        if len(self.structures_lattice) > 0:
+           generate_nscf_soc_folders(self.structures_lattice, saxis, {**user_incar, **user_incar_custom})
+
 
 ####         STEP 1           ###
 # customize the default settings for INCAR file
-#user_incar_custom = {"ALGO":"Normal","LDAUU":{"Eu":6.0},"LDAUL":{"Eu":3, "Sb":-1, "O":-1},"EDIFF":5e-8, "LWAVE":False,"LVHAR":False,"LAECHG":False,"LREAL":False,"NPAR":4,"ICHARG":2}
+#user_incar_scf_custom = {"LDAUU":{"Eu":6.0},"LDAUL":{"Eu":3, "Sb":-1, "O":-1}}
 
 #fdt = FDTcalculations(filename='/home/ucl/modl/mmarkov/soft/FDToolbox_py3_dev/TEST_FOLDER/POSCAR', disp_ion=0.005, disp_lattice=0.00)
-#fdt.generate_scf(user_incar=user_incar_custom)
+#fdt.generate_scf(user_incar=user_incar_scf_custom)
 
 #folders = list(fdt.structures_ion.keys())
 #generate_submit_manneback(folders, nprocs = 16, path_to_exec="/home/ucl/modl/mmarkov/soft/vasp/vasp.5.4.1_couplageEM/bin/vasp_std")
@@ -156,7 +208,12 @@ class FDTcalculations(object):
 fdt = FDTcalculations(filename='/home/ucl/modl/mmarkov/soft/FDToolbox_py3_dev/TEST_FOLDER/POSCAR', disp_ion=0.005, disp_lattice=0.00)
 
 scf_completed = fdt.check_scf()
-print('Scf calculations completed succesfully? ', scf_completed)
+print('All SCF calculations completed succesfully? ', scf_completed)
 
-#if scf_completed:
-#   fdt.generate_polarization_old()
+user_incar_nscf_custom = {"LDAUU":{"Eu":6.0},"LDAUL":{"Eu":3, "Sb":-1, "O":-1}}
+saxis = (0,0,1)
+
+if scf_completed:
+   fdt.generate_old_polarization()
+   fdt.generate_new_polarization()
+   fdt.generate_nscf_soc(saxis, user_incar_nscf_custom)
